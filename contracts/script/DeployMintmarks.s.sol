d87f1cd71ae31d95f546133ac59bcabd9b633948
0x471 <0x471@protonmail.com>
2025-12-04 00:47:57 +0000

    chore(contract): update deploy script for owner param

--- contracts/script/DeployMintmarks.s.sol
+++ contracts/script/DeployMintmarks.s.sol
@@ -8,14 +8,18 @@ contract DeployMintmarksScript is Script {
     function run() external returns (Mintmarks) {
         address emailVerifier = vm.envAddress("EMAIL_VERIFIER_ADDRESS");
         address passportVerifier = vm.envAddress("ZKPASSPORT_VERIFIER_ADDRESS");
+        // Owner defaults to deployer, can be overridden with MINTMARKS_OWNER
+        address owner = vm.envOr("MINTMARKS_OWNER", msg.sender);
 
         vm.startBroadcast();
-        Mintmarks mintmarks = new Mintmarks(emailVerifier, passportVerifier);
+        Mintmarks mintmarks = new Mintmarks(emailVerifier, passportVerifier, owner);
         vm.stopBroadcast();
 
         console.log("Mintmarks deployed at:", address(mintmarks));
         console.log("Email verifier:", emailVerifier);
         console.log("Passport verifier:", passportVerifier);
+        console.log("Owner:", owner);
+        console.log("Luma pubkey hash:", vm.toString(mintmarks.LUMA_PUBKEY_HASH()));
         console.log("");
         console.log("Add to .env:");
         console.log("MINTMARKS_ADDRESS=%s", address(mintmarks));
